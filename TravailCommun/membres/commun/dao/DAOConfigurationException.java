b5371d5d60316fb85821df36d3932a279119fb11
abdouwake <abdouwake@users.noreply.github.com>
2019-03-08 11:26:57 +0100

    La version 1 du projet
    Vous trouverez tout le projet eclipse à télécharger.

---
+++ TravailCommun/membres/commun/dao/DAOConfigurationException.java
@@ -0,0 +1,23 @@
+package membres.commun.dao;
+
+
+public class DAOConfigurationException extends RuntimeException {
+	
+/*
+* Constructeurs
+*/
+	
+public DAOConfigurationException( String message ) {
+super( message );
+}
+
+public DAOConfigurationException( String message, Throwable
+cause ) {
+super( message, cause );
+}
+
+public DAOConfigurationException( Throwable cause ) {
+super( cause );
+}
+
+}
